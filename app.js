b632a66e5aa876f53b40dc1c5377a00443b81d00
Luis de la Cal <luiscal4a@gmail.com>
2019-02-04 16:27:57 +0100

    Added CORS headers to prevent problems in browser petitions

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ app.use(cors())
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit: 5000000}));
 
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', ['Content-Type', '*'])
+  next();
+});
+
 app.use('/user', userRoutes);
 app.use('/book', bookRoutes);
 
